97dbc83f60fea9306e065f9cef5ce63d1ecf15fd
Binoy Pilakkat <binoypilakkat@hotmail.com>
2014-07-30 22:16:37 +0530

    Class Programming Started
    Got first output graph

--- led_graphics.py
+++ led_graphics.py
@@ -1,12 +1,40 @@
 # This is the main file
 import re,subprocess,shlex
+from datetime import date
+class dateArray:
+    """
+    A class to hold date values in List
+    add date as string  yyy/m/dd
+    
+    
+    """
+    def __init__(self):
+        self.array =[]
+    def add(self,dt):
+        ldt =[int(i) for i in dt.split("/")]
+        self.array.append(date(ldt[0],ldt[1],ldt[2]))
+    def __str__(self):
+        out =""
+        for dt in self.array:
+            out += dt.strftime("%A %d-%m-%y") + '\n'
+        return out
+    def __getitem__(self,index):
+        return self.array[index]
+    def __call__(self):
+        return self.array
+    
 def led_argParser(arg):
+    """
+    Return output of ledger query as list
+    arg are ledger arguements
+    
+    """
     def line2list():
         p = re.compile('[^\s]+')
         return p.findall
     arg = "ledger "+ arg
     args = shlex.split(arg)
-
+    
     p = subprocess.Popen(args,stdout= subprocess.PIPE,stdin = subprocess.PIPE,\
                          shell= True)
     count = 0
@@ -16,4 +44,34 @@ def led_argParser(arg):
         outList.append(f(i))
         count += 1
     return outList
-
+    
+class LedgerPlotter:
+    def __init__(self,args):
+        self.ledger_out_as_list = led_argParser(args)
+        
+    def BalanceSummary(self,arg):
+        """
+        Gets the status of Total Expenses in Detail
+        Input arguements should start with -s bal [arguements]      
+        """        
+        arg = '-s bal '+arg
+        print arg
+        self.ledger_out_as_list = led_argParser(arg)
+        
+        reqd_Data   = self.ledger_out_as_list[0:-2] #crops to required data
+        #amount Rs AccName
+        dictBarChart = {}
+        for line in reqd_Data:
+            (amount,AccountName) = (line[0],line[2])
+            dictBarChart[AccountName]=amount
+        print dictBarChart    
+        for i,key in enumerate(dictBarChart):
+            if float(dictBarChart[key]) <= 0:
+                c = 'red'
+            else:
+                c = 'green'
+            plt.bar(i,float(dictBarChart[key]),color=c,alpha =0.5)
+        plt.xticks(np.arange(len(dictBarChart))+0.4, dictBarChart.keys(),\
+        rotation =270)
+        plt.ylabel("Rupees")
+        plt.show()   
--- test.py
+++ test.py
@@ -1,52 +1,58 @@
-import shlex, subprocess, re
-from datetime import date
+
+import matplotlib.pyplot as plt
 import numpy as np
-class dateArray:
-    def __init__(self):
-        self.array =[]
-    def add(self,dt):
-        ldt =[int(i) for i in dt.split("/")]
-        self.array.append(date(ldt[0],ldt[1],ldt[2]))
-    def __str__(self):
-        out =""
-        for dt in self.array:
-            out += dt.strftime("%A %d-%m-%y") + '\n'
-        return out
-    def __getitem__(self,index):
-        return self.array[index]
-    def __call__(self):
-        return self.array
-    
-def led_argParser(arg):
-    def line2list():
-        p = re.compile('[^\s]+')
-        return p.findall
-    arg = "ledger "+ arg
-    args = shlex.split(arg)
-    
-    p = subprocess.Popen(args,stdout= subprocess.PIPE,stdin = subprocess.PIPE,\
-                         shell= True)
-    count = 0
-    f= line2list()
-    outList =[]
-    for i in p.stdout:
-        outList.append(f(i))
-        count += 1
-    return outList
-dArr = dateArray()
-vArr =[]   
-for line in  led_argParser("-n -p 'every day' reg ^Exp"):
-    print line
-    dArr.add(line[0])
-    vArr.append(float(line[4]))
+from led_graphics import dateArray,led_argParser
+
+
+
+
+
+
+class LedgerPlotter:
+    def __init__(self,args):
+        self.ledger_out_as_list = led_argParser(args)
+        
+    def BalanceSummary(self,arg):
+        """
+        Gets the status of Total Expenses in Detail
+        Input arguements should start with -s bal [arguements]      
+        """        
+        arg = '-s bal '+arg
+        print arg
+        self.ledger_out_as_list = led_argParser(arg)
+        
+        reqd_Data   = self.ledger_out_as_list[0:-2] #crops to required data
+        #amount Rs AccName
+        dictBarChart = {}
+        for line in reqd_Data:
+            (amount,AccountName) = (line[0],line[2])
+            dictBarChart[AccountName]=amount
+        print dictBarChart    
+        for i,key in enumerate(dictBarChart):
+            if float(dictBarChart[key]) <= 0:
+                c = 'red'
+            else:
+                c = 'green'
+            plt.bar(i,float(dictBarChart[key]),color=c,alpha =0.5)
+        plt.xticks(np.arange(len(dictBarChart))+0.4, dictBarChart.keys(),rotation =270)
+        plt.ylabel("Rupees")
+        plt.show()    
+        
+        
+
+
+
+#    dArr.add(line[0])
+#    vArr.append(float(line[0]))
+#    xArr.append()
     
 
-for (d,v) in zip(dArr,vArr):
-    print d,v
+#for (d,v) in zip(dArr,vArr):
+#    print d,v
     
-import matplotlib.pyplot as plt
-plt.plot(dArr(),vArr,'-')
-plt.xlabel('Day')
-plt.ylabel('Expense in Rs')
-plt.show()
-#     
+
+
+if __name__ == "__main__":
+    lplot = LedgerPlotter('-s bal ^Exp ')
+    lplot.BalanceSummary('Air Food Lia')
+    
